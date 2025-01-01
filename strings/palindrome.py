b89cc5132f759dab679c049f846cad27b0c62b70
Prasai1488 <prasaiprithvi4@gmail.com>
2025-01-01 18:48:51 +0545

    palindrome with two pointers method

--- strings/palindrome.py
+++ strings/palindrome.py
@@ -1,20 +1,41 @@
 # Check palindrome
 
-def reverse_string(s):
-    return s[::-1]
+# def reverse_string(s):
+#     return s[::-1]
+
+# def check_palindrome(string):
+#     reversedString = reverse_string(string)
+#     if reversedString == string:
+#         return True 
+#     return False 
+
+# def main():
+#     string = "olo"
+#     if check_palindrome(string):
+#         print(f"'{string}' is a palindrome.")
+#     else:
+#         print(f"'{string}' is not a palindrome.")
+
+
+# main()
+
+
+# Time Complexity : O(n)
+# Space complexity : O(n)
+
+
+
+# ? Alternative approach : 2 pointers method
 
 def check_palindrome(string):
-    reversedString = reverse_string(string)
-    if reversedString == string:
-        return True 
-    return False 
+    left,right = 0, len(string) - 1
+    while left < right:
+        if string[left] != string[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
 
-def main():
-    string = "dad"
-    if check_palindrome(string):
-        print(f"'{string}' is a palindrome.")
-    else:
-        print(f"'{string}' is not a palindrome.")
+# Time complexity : O(n)
+# Space complexity : O(1)
 
-if __name__ == "__main__":
-    main()
